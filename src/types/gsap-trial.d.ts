b206a5b40af0925c5f994a558da3fa9aa928d086
Abhishek jain <abhishekjain@Abhisheks-MacBook-Pro.local>
2026-03-20 10:47:27 +0530

    DEPloyment updates

---
+++ src/types/gsap-trial.d.ts
@@ -0,0 +1,11 @@
+declare module "gsap-trial/SplitText" {
+  export class SplitText {
+    constructor(target: any, vars?: any);
+
+    chars: HTMLElement[];
+    words: HTMLElement[];
+    lines: HTMLElement[];
+
+    revert(): void;
+  }
+}
